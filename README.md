9e194622c439267fa007882778e0ad948ddef76b
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2015-07-20 00:37:46 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# NAME
+
+App::hiki2md - convert hiki document to markdown.
+
+# SYNOPSIS
+
+    hiki2md input_hiki > output.md
+
+# DESCRIPTION
+
+App::hiki2md is ...
+
+# TODO
+
+- 整形済みテキスト
+- テーブル
+- 文字装飾
+- 引用
+- 用語定義
+- コメント
+
+# LICENSE
+
+Copyright (C) SUZUKI Masashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
---
+++ lib/App/hiki2md.pm
@@ -0,0 +1,57 @@
+package App::hiki2md;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::hiki2md - convert hiki document to markdown.
+
+=head1 SYNOPSIS
+
+    hiki2md input_hiki > output.md
+
+=head1 DESCRIPTION
+
+App::hiki2md is ...
+
+=head1 TODO
+
+=over 4
+
+=item * 整形済みテキスト
+
+=item * テーブル
+
+=item * 文字装飾
+
+=item * 引用
+
+=item * 用語定義
+
+=item * コメント
+
+=back
+
+=head1 LICENSE
+
+Copyright (C) SUZUKI Masashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+SUZUKI Masashi E<lt>m15.suzuki.masashi@gmail.comE<gt>
+
+=cut
+
